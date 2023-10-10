f1985cfc90f443bf1b3a2d065e2b7ecd0dcc8b0e
Comfort99 <shingangecomfort@gmail.com>
2023-10-10 07:21:11 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Main point 
+ * descpription - Print _putchar
+ * Return 0
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('C');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
