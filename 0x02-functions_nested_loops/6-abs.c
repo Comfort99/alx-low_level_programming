b3e9ecc1399d8cbeb96259b5c9bc9c8e70fcee4e
Comfort99 <shingangecomfort@gmail.com>
2023-10-10 14:59:27 -0700

    nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _abs -function that computes absolute values of an integer
- * @a - parameter to be checked
+ * @a: parameter to be checked
+ * Description : This function returns absolute value of a number
  * Return: -a or a
  */
 int _abs(int a)
 {
-	if (a < 0)
+	if (a <= 0)
 	{
 		return (-a);
 	}
