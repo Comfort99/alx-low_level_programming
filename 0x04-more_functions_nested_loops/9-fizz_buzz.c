48e2a65dc276a2d08e742f9a9cfbff6d3b8feca0
Comfort99 <shingangecomfort@gmail.com>
2023-10-12 16:40:06 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,33 +6,24 @@
  */
 int main(void)
 {
-	int num;
+	int i;
+	char a[] = "Fizz";
+	char b[] = "Buzz";
+	char c[] = "FizzBuzz";
 
-	for (num = 1; num <= 100; num++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz");
-		}
-		else if ((num % 5) == 0)
-		{
-			printf("Buzz");
-		}
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", c);
+		else if (i % 3 == 0)
+			printf("%s ", a);
+		else if (i % 5 == 0)
+			printf("%s ", b);
 		else
-		{
-			printf("%d", num);
-		}
-		if (num == 100)
-		{
-			continue;
-			printf(" ");
-		}
+			printf("%d ", i);
 	}
 	printf("\n");
-
-	return  (0);
+	return (0);
 }
