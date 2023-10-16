e9c1a60aa03ef833dca8754bbcfd25eb94c0f468
Comfort99 <shingangecomfort@gmail.com>
2023-10-16 14:58:24 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,21 +5,15 @@
  */
 void print_rev(char *s)
 {
-	char c;
 	int length = 0;
 	int i;
 
 	for(i = 0; s[i] != '\0'; i++)
-	{
 		length++;
-	}
 
-	s--;
-
-	for (i = 0; i < length / 2; i++)
+	for (i = length - 1; i >= 0; i--)
 	{
-		c = s[i];
-		s[i] = s[length -i -1];
-		s[length -i -1] = c;
+		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
