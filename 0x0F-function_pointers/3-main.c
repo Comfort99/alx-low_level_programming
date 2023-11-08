e0b0de8283e8ee9c2ad7dc0699eab39b482d997a
Comfort99 <shingangecomfort@gmail.com>
2023-11-08 08:09:54 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,43 +8,32 @@
  * @argv: An array of pointers to arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
-
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	switch (argv[2][0])
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-		case '%':
-			if (num2 == 0)
-			{
-				printf("Error\n");
-				return (100);
-			}
-			result = (argv[2][0] == '/') ? num1 / num2 : num1 % num2;
-			break;
-		default:
-			printf("Error\n");
-			return (99);
+		printf("Error\n");
+		exit(99);
 	}
-	printf("%d\n", result);
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
+
