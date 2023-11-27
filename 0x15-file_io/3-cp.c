74da1a8622f677e419d7e1b13b68bea7e77b8dd4
Comfort99 <shingangecomfort@gmail.com>
2023-11-27 12:48:00 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,60 +2,52 @@
 /**
  * check_IO_stat - function that checks the access permissions of a file
  * @stat: file descriptor
+ * @fd: file descriptor
  * @filename: name of a file
  * @mode: closing or opening
- * @fd: file descriptor
  */
-void check_IO_stat(int stat, int fd, char *filename, char mode)
+void check_IO_stat(int stat, int fd, const char *filename, char mode)
 {
-	if (mode == 'C' && stat == -1)
+	if (stat == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		if (mode == 'C')
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		else if (mode == 'O')
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		else if (mode == 'W')
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(100);
-	}
-	else if (mode == 'O' && stat == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	else if (mode == 'W' && stat == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
 	}
 }
 
 /**
  * main - function to copy content of one file to another
- * @arg: arguments
- * @argv: arguments passed
- * Return: 1 on success, terminate otherwise
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, terminate otherwise
  */
-int main(int arg, char *argv[])
+int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, len;
+	int src_fd, dest_fd, n_read, n_written;
 	char buffer[1024];
 
-	if (arg != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	check_IO_stat(fd_from, -1, argv[1], 'O');
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+	src_fd = open(argv[1], O_RDONLY);
+	check_IO_stat(src_fd, -1, argv[1], 'O');
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	check_IO_stat(fd_to, -1, argv[2], 'W');
-	while ((len = read(fd_from, buffer, sizeof(buffer))) > 0)
+	check_IO_stat(dest_fd, -1, argv[2], 'W');
+	while ((n_read = read(src_fd, buffer, sizeof(buffer))) > 0)
 	{
-		if (write(fd_to, buffer, len) != len)
-			check_IO_stat(-1, -1, argv[2], 'W');
+		n_written = write(dest_fd, buffer, n_read);
+		check_IO_stat(n_written, -1, argv[2], 'W');
 	}
-	if (len == -1)
-		check_IO_stat(-1, -1, argv[1], 'O');
-	if (close(fd_from) == -1)
-		check_IO_stat(-1, fd_from, NULL, 'C');
-	if (close(fd_to) == -1)
-		check_IO_stat(-1, fd_to, NULL, 'C');
+	check_IO_stat(n_read, -1, argv[1], 'O');
+	check_IO_stat(close(src_fd), src_fd, NULL, 'C');
+	check_IO_stat(close(dest_fd), dest_fd, NULL, 'C');
 	return (0);
 }
