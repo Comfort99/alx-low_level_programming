cc5f310f40923be3f96cb83e5e27a449064a3af9
Comfort99 <shingangecomfort@gmail.com>
2023-11-27 12:22:36 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,71 +1,61 @@
 #include "main.h"
 /**
- * cp - A function that copies the content
- * of the file_from to file_to.
- * @file_from: The file to be copied from.
- * @file_to: The file to be copied to.
- * Return: 0 on success, 1 on failure.
+ * check_IO_stat - function that checks the access permissions of a file
+ * @stat: file descriptor
+ * @filename: name of a file
+ * @mode: closing or opening
+ * @fd: file descriptor
  */
-int cp(const char *file_from, const char *file_to)
+void check_IO_stat(int stat, int fd, char *filename, char mode)
 {
-	int fd_from, fd_to;
-	int bytes_read;
-	char buf[BUF_SIZE];
-
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	if (mode == 'C' && stat == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		return (1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
-	fd_to = open(file_to, O_WRONLY | O_TRUNC, 0666);
-	if (fd_to == -1)
+	else if (mode == 'O' && stat == -1)
 	{
-		close(fd_from);
-		dprintf(2, "Error: Can't write to file %s\n", file_to);
-		return (1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
 	}
-	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+	else if (mode == 'W' && stat == -1)
 	{
-		if (write(fd_to, buf, bytes_read) != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			dprintf(2, "Error: Can't write to file %s\n", file_to);
-			return (1);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
 	}
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		return (1);
-	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		dprintf(2, "Error: Can't close file descriptor %d\n",
-				fd_from != -1 ? fd_from : fd_to);
-	return (1);
-
-	return (0);
 }
 
 /**
- * main - Entry point
- * @argc: The number of arguments.
- * @argv: The arguments.
- * Return: 0 on success, 1 on failure.
+ * main - function to copy content of one file to another
+ * @arg: arguments
+ * @argv: arguments passed
+ * Return: 1 on success, terminate otherwise
  */
-int main(int argc, char **argv)
+int main(int arg, char *argv[])
 {
-	if (argc != 3)
+	int fd_from, fd_to, len;
+	char buffer[1024];
+
+	if (arg != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		return (97);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
 	}
-	if (cp(argv[1], argv[2]) == 1)
+	fd_from = open(argv[1], O_RDONLY);
+	check_IO_stat(fd_from, -1, argv[1], 'O');
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	check_IO_stat(fd_to, -1, argv[2], 'W');
+	while ((len = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		return (99);
+		if (write(fd_to, buffer, len) != len)
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
+	if (len == -1)
+		check_IO_stat(-1, -1, argv[1], 'O');
+	if (close(fd_from) == -1)
+		check_IO_stat(-1, fd_from, NULL, 'C');
+	if (close(fd_to) == -1)
+		check_IO_stat(-1, fd_to, NULL, 'C');
 	return (0);
 }
